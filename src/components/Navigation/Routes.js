3d75bec8bacdd21b7ea60f34940a735365fea8a0
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-10-25 12:43:31 -0400

    Create Routes component

---
+++ src/components/Navigation/Routes.js
@@ -0,0 +1,24 @@
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {Contact} from './Contact'
+import {NavBar} from './NavBar'
+import {Home} from './Home'
+import {DownloadResume} from './DownloadResume'
+import {ProjectPage} from './ProjectPage'
+
+export const Routes = () => {
+
+    return (
+        <Router>
+            <NavBar/>
+            <section className="App">
+            <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route exact path='/contact' component={Contact}/>
+                <Route exact path='/projects' component={ProjectPage}/>
+                <Route exact path='/download_resume' component={DownloadResume}/>
+            </Switch>
+            </section>
+        </Router>
+    )
+}
+
