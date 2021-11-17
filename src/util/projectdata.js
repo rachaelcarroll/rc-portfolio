19c07a79250bffe814a7378a3609c4ff50ca92c1
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-11-17 16:09:01 -0500

    add project data file

---
+++ src/util/projectdata.js
@@ -0,0 +1,90 @@
+export const projectData = [
+    {
+      name: 'ClienTell',
+      image: ,
+      summary: 'A platform that empowers sex workers to document and share their date experiences',
+      tech: 'GraphQL, ApolloClient, React, Router, Circle CI, JavaScript, SCSS, HTML5, Cypress, Netlify',
+      accomplishments: 'Self-taught GraphQL and working on a full-stack team',
+      timeline: '3 weeks',
+      deployedLink: 'https://clientell.netlify.app/',
+      repoLink: 'https://github.com/rachaelcarroll/date-em-rate-em-fe-1',
+      id: 1
+    },
+    {
+      name: 'OnlyDevs',
+      image: ,
+      summary: 'A question-answer forum for technical front end development concepts',
+      tech: 'React, Router, React Hooks, Cypress, SCSS, PostgreSQL, Express.js, Heroku',
+      accomplishments: 'Self-taught backend technologies',
+      timeline: '10 days',
+      deployedLink: 'https://onlydevs.herokuapp.com/',
+      repoLink: 'https://github.com/rachaelcarroll/only-devs',
+      id: 2
+    },
+    {
+      name: 'The Scene',
+      image: ,
+      summary: 'An application that allows a user to shuffle and save amusing conversation starters',
+      tech: 'React, Router, React Hooks, JavaScript, CSS3, HTML5, Cypress, REST API, Vercel',
+      accomplishments: 'Implemented React Hooks',
+      timeline: '1 week',
+      deployedLink: '',
+      repoLink: '',
+      id: 3
+    },
+    {
+      name: 'Portfolio',
+      image: ,
+      summary: 'The portfolio site that you are currently viewing',
+      tech: 'React, JavaScript, HTML5, SCSS, Google Analytics, Vercel',
+      accomplishments: 'Implemented Google Analytics to track site traffic',
+      deployedLink: 'https://www.rcarroll.co/',
+      repoLink: 'https://github.com/rachaelcarroll/rc-portfolio',
+      id: 4
+    },
+    {
+      name: 'Rancid Tomatillos',
+      image: ,
+      summary: 'A movie browsing application',
+      tech: 'React, React Router, Cypress, JavaScript, CSS3, HTML5, Vercel, Miro',
+      accomplishments: 'First application created with React and Cypress',
+      timeline: '10 days',
+      deployedLink: 'https://rancid-tomatillos-one.vercel.app/',
+      repoLink: 'https://github.com/rachaelcarroll/Rancid-Tomatillos',
+      id: 5
+    },
+    {
+      name: 'The Overlook',
+      image: ,
+      summary: 'An application to book and display hotel reservations',
+      tech: 'JavaScript, HTML5, CSS3/SCSS, WebPack, Fetch API, Day.js, Lighthouse, Sketch',
+      accomplishments: 'Robust unit testing, GET & POST network requests, Accessibility rating of 100',
+      timeline: '6 days',
+      deployedLink: '',
+      repoLink: 'https://github.com/rachaelcarroll/overlook-hotel',
+      id: 6
+    },
+    {
+        name: 'Rosemary & Sage',
+        image: ,
+        summary: 'An application to view, favorite, and search through a collection of recipes',
+        tech: 'HTML5, CSS3, Mocha, Chai, Webpack',
+        accomplishments: 'Utilized array prototype methods to perform data manipulation',
+        timeline: '1 week',
+        deployedLink: 'https://www.youtube.com/watch?v=JDhKCZePFT0',
+        repoLink: 'https://github.com/rachaelcarroll/Whats-Cookin',
+        id: 7
+      },
+    {
+        name: 'Ranch!',
+        image: ,
+        summary: 'A static comp for Ranch rankings',
+        tech: 'HTML5, CSS3',
+        accomplishments: 'DRY and semantic HTML and CSS, responsive across various screen sizes',
+        timeline: '5 days',
+        deployedLink: 'https://rachaelcarroll.github.io/static-comp/',
+        repoLink: 'https://github.com/rachaelcarroll/static-comp',
+        id: 8
+      }
+
+  ]
