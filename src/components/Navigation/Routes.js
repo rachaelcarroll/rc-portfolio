4867e7d59f393e8bea031d46dc930a4e18d968fb
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-11-18 12:50:30 -0500

    Remove nav and footer from routes component

--- src/components/Navigation/Routes.js
+++ src/components/Navigation/Routes.js
@@ -1,6 +1,5 @@
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import {Contact} from '../Contact';
-import {NavBar} from './NavBar';
 import {Home} from '../Home';
 import {Projects} from '../Projects';
 
@@ -8,7 +7,6 @@ export const Routes = () => {
 
     return (
         <Router>
-            <NavBar/>
             <Switch>
                 <Route exact path='/' component={Home}/>
                 <Route exact path='/projects' component={Projects}/>
