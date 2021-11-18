19180b31c438d5567cf91efe15e896171253d52c
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-11-18 13:17:19 -0500

    move BrowserRouter to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './Components/App';
+import {BrowserRouter, BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './styles/styles.scss';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
