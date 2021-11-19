c8b43eedf2e4f9219d9784d23f2d40c1d0a21990
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-11-18 20:14:41 -0500

    update class naming on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,10 @@ export const Footer = () => {
     return (
         <footer className='footer'>
         <section className='footer-links'>
-            <a href ='mailto:rachaelcarroll.m@gmail.com'><img src={"https://i.ibb.co/HCqybSP/mail-2.png"} className='footer-links'/></a>  
-            <a href ='https://github.com/rachaelcarroll' target='_blank'><img src={"https://i.ibb.co/NyYsL14/github-2.png"} className='footer-links'/></a>  
-            <a href ='https://www.linkedin.com/in/rachaelcarroll/' target='_blank'><img src={"https://i.ibb.co/fqvzWXm/linkedin-4.png"} className='footer-links'/></a>  
+            <a href ='mailto:rachaelcarroll.m@gmail.com'><img src={"https://i.ibb.co/HCqybSP/mail-2.png"} className='footer-icon'/></a>  
+            <a href ='https://github.com/rachaelcarroll' target='_blank'><img src={"https://i.ibb.co/NyYsL14/github-2.png"} className='footer-icon'/></a>  
+            <a href ='https://www.linkedin.com/in/rachaelcarroll/' target='_blank'><img src={"https://i.ibb.co/fqvzWXm/linkedin-4.png"} className='footer-icon'/></a>  
         </section>
-        <hr>
-        </hr>
         <p>designed and developed by rachael carroll</p>
         </footer>
     )
