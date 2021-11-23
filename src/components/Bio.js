4ad83d4d3be893e3b207c16b8335f070f33a88ef
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-11-23 15:35:02 -0500

    Update bio with aboutMe data

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,22 +1,46 @@
 import { Link } from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
+import { about } from '../util/aboutMe';
 
 export const Bio = () => {
 
     return (
-        <section className='about' id='about'>
-            <Fade left>
-                <div className='headshot'>this will be my photo!</div>
-            </Fade>
-            <div className='bio-and-resume'>
-                <p className='bio'>this will be where my bio goes, yay!</p>
-                <a href='https://drive.google.com/file/d/1R7JtGgg4RKQzCGywsf0Lq_EiJcfYbUnk/view?usp=sharing'> 
-                    <button className='resume'>resume</button>
-                </a>
-                <Link to='/contact'>
-                    <button>contact me</button>
-                </Link>
-            </div>
-        </section>
+        <main id='about' className='about'>
+                <Fade bottom duration={1000} delay={300} distance="0px">
+                  <h2 className='about-header'>About Me</h2>
+                </Fade>
+                <section className='headshot-and-bio'>
+                    <Fade bottom duration={1000} delay={600} distance="30px">
+                    <div className='headshot'>
+                        <img alt='profile picture' src='https://i.imgur.com/PTHFbBC.png' />
+                    </div>
+                    </Fade>
+                    <Fade left duration={1000} delay={1000} distance='30px'>
+                    <article className='bio'>
+                    <div className='bio-details'>
+                        <p className='bio-text'>{about.paragraph1}</p>
+                        <p className='bio-text'>{about.paragraph2}</p>
+                        <p className='bio-text'>{about.paragraph3}</p>
+                        <span className='bio-btns'>
+                            <a
+                            target="_blank"
+                            href='https://drive.google.com/file/d/1R7JtGgg4RKQzCGywsf0Lq_EiJcfYbUnk/view?usp=sharing'
+                            >
+                                <button className='bio-btn'>
+                                    resume
+                                </button>
+                            </a>
+                            <Link to='/contact'>
+                             <button
+                             className='bio-btn'>
+                                 contact me
+                             </button>
+                            </Link>
+                        </span>
+                    </div>
+                    </article>
+                    </Fade>
+                </section>
+        </main>
     )
 }
