b9d2e4340e7aab67ecef6f01fa15e0caeb051402
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-11-23 15:35:40 -0500

    create scroll fade on navbar

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -1,17 +1,36 @@
 import { HashLink } from 'react-router-hash-link';
 import { NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 export const NavBar = () => {
+
+    const [navBar, setNavBar] = useState(false);
+
+    
+    useEffect(() => {
+        changeBackground()
+        window.addEventListener("scroll", changeBackground)
+    })
+
+    const changeBackground = () => {
+        console.log(window.scrollY)
+        if (window.scrollY >= 35) {
+          setNavBar(true)
+        } else {
+          setNavBar(false)
+        }
+      }
+
     return (
-        <section className='nav'>
+        <section className={navBar ? 'nav-active' : 'nav'}>
             <HashLink smooth to='/#about'>
-                <p className='aboutBtn'>about</p>
+                <p className='navbtn'>about</p>
             </HashLink>
             <NavLink to='/projects'>
-                <p className='projectsBtn'>projects</p>
+                <p className='navbtn'>projects</p>
             </NavLink>
             <NavLink to='/contact'>
-                <p className='contactBtn'>contact</p>
+                <p className='navbtn'>contact</p>
             </NavLink>
         </section>
     )
