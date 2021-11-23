fe96e5dafe16b2478b23568a7e2d35e1b2ecd143
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-11-23 11:11:11 -0500

    Update nav hovers

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -5,13 +5,13 @@ export const NavBar = () => {
     return (
         <section className='nav'>
             <HashLink smooth to='/#about'>
-                <button className='aboutBtn'>about</button>
+                <p className='aboutBtn'>about</p>
             </HashLink>
             <NavLink to='/projects'>
-                <button className='projectsBtn'>projects</button>
+                <p className='projectsBtn'>projects</p>
             </NavLink>
             <NavLink to='/contact'>
-                <button className='contactBtn'>contact</button>
+                <p className='contactBtn'>contact</p>
             </NavLink>
         </section>
     )
