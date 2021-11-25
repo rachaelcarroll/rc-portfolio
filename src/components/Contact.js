7078d814be1a44431392d273af48bb7250e88dfc
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-11-24 19:56:47 -0500

    Build Contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+
+export const Contact = () => {
+
+    return (
+        <section className='contact'>
+            <h3>Let’s start a conversation. It could lead to the next great idea.</h3>
+        </section>
+    )
+}
