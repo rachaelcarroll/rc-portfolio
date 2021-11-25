d8d2c0f7cbf64d15089373a66f99923b2dd7086f
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-11-24 19:57:18 -0500

    Update imports in Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,15 @@
-import {Routes, Route} from 'react-router-dom';
-import {Contact} from '../Contact';
-import {Home} from '../Home';
-import {Projects} from '../Projects';
+import { Routes, Route } from 'react-router-dom';
+import { Contact } from '../Contact';
+import { Home } from '../Home';
+import { Projects } from '../Projects';
 
 export const Navigation = () => {
-
     return (
-            <Routes>
-                <Route exact path='/' element={<Home />}/>
-                <Route exact path='/projects' element={<Projects />}/>
-                <Route exact path='/contact' element={<Contact />}/>
-            </Routes>
+        <Routes>
+            <Route exact path='/' element={<Home />}/>
+            <Route exact path='/projects' element={<Projects />}/>
+            <Route exact path='/contact' element={<Contact />}/>
+        </Routes>
     )
 }
 
