de51336f1287e87f44d25bd6916de57216149028
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-12-08 21:57:31 -0500

    Update h3 in Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ export const Contact = () => {
 
     return (
         <section className='contact'>
-            <h3>Let’s start a conversation. It could lead to the next great idea.</h3>
+            <h3>Let’s start a conversation. Who knows, it could lead to the next great idea.</h3>
         </section>
     )
 }
