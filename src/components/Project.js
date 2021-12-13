5c0076de364db95ac918fc144d120893729ea776
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-12-13 17:04:13 -0500

    Update project classes for styling and flip card effect

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import github from '../images/github.png';
 import web from '../images/web.png';
 
@@ -5,21 +6,23 @@ export const Project = ({ project }) => {
 
     return (
         <article className='project-card'>
-            <img
-            className='project-preview'
-            alt='project preview'
-            src={project.image}
-            />
-            <div className='project-details'>
-                <h3>{project.name}</h3>
-                <p className='project-text'>{project.summary}</p>
-                <p className='project-text'>{project.tech}</p>
-                {/* <p className='project-text'>{project.accomplishments}</p> */}
-                <div>
-                    <a href={project.repoLink}><img alt='github logo' src={github}/></a>
-                    {project.deployedLink !== '' && (
-                    <a href={project.deployedLink}><img alt={`${project.name} website icon`} src={web}/></a>
-                    )}
+            <div class='project-card-inner'>
+                <img
+                className='project-card-front'
+                alt='project preview'
+                src={project.image}
+                />
+                <div className='project-card-back'>
+                    <h3>{project.name}</h3>
+                    <p className='project-text'>{project.summary}</p>
+                    <p className='project-text'>{project.tech}</p>
+                    {/* <p className='project-text'>{project.accomplishments}</p> */}
+                    <div className='project-links'>
+                        <a href={project.repoLink}><img alt='github logo' src={github}/></a>
+                        {project.deployedLink !== '' && (
+                        <a href={project.deployedLink}><img alt={`${project.name} website icon`} src={web}/></a>
+                        )}
+                    </div>
                 </div>
             </div>
         </article>
