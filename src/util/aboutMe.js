9eac2c684c27349dbd7070c796c4a0a4d1951c75
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-12-14 11:49:50 -0500

    Update bio

--- src/util/aboutMe.js
+++ src/util/aboutMe.js
@@ -1,6 +1,6 @@
 export const about = {
-    paragraph1: "I'm a software developer who is passionate about all things user experience, interaction, and design. My goal as a developer is to leverage technology to build things that matter.",
-    paragraph2: "I am a recent graduate of Turing School of Software and Design where I gained experience building reliable, thoroughly tested apps individually and on teams. I thrive in the balance of logic and fluidity of design and language that software engineering provides and I've grown by learning how others create and interpret code.",
-    paragraph3: "I'm currently seeking a developer role in an inclusive environment that upholds authentic and empathetic practices and I am eager to work with others that align with these values. And when I'm not pushing up code, you can most likely find me outside with my schnauzer, searching for the best burrito in town, or geeking out on home design & vintage finds."
+    paragraph1: "I'm a software developer who is passionate about leveraging technology to create a seamless user experience through innovative and thoughtful user interface design and development.",
+    paragraph2: "I graduated from Turing School of Software and Design where I studied front end web development and gained experience building reliable, thoroughly tested apps individually and on teams. I thrive in the balance of logic and fluidity of design and language that software engineering provides and I grow by learning how others create and interpret code.",
+    paragraph3: "I'm currently seeking a developer role in an inclusive environment that upholds authentic and empathetic practices and I am eager to work with others that align with these values. And when I'm not pushing up code, you can most likely find me with my schnauzer, Albus, searching for the best burrito in town, or geeking out on home design & vintage finds."
 }
 
