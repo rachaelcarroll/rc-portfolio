0fb325b04633dabec1d212e63c9e1c9d3df67152
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-12-15 16:22:07 -0500

    Update bio

--- src/util/aboutMe.js
+++ src/util/aboutMe.js
@@ -1,6 +1,6 @@
 export const about = {
-    paragraph1: "I'm a software developer who is passionate about leveraging technology to create a seamless user experience through innovative and thoughtful user interface design and development.",
+    paragraph1: "I'm a software developer passionate about leveraging technology to create a seamless user experience through innovative and thoughtful user interface design and development.",
     paragraph2: "I graduated from Turing School of Software and Design where I studied front end web development and gained experience building reliable, thoroughly tested apps individually and on teams. I thrive in the balance of logic and fluidity of design and language that software engineering provides and I grow by learning how others create and interpret code.",
-    paragraph3: "I'm currently seeking a developer role in an inclusive environment that upholds authentic and empathetic practices and I am eager to work with others that align with these values. And when I'm not pushing up code, you can most likely find me with my schnauzer, Albus, searching for the best burrito in town, or geeking out on home design & vintage finds."
+    paragraph3: "Prior to software development, I ran the marketing and operations of an all women commercial real estate team, worked countless hours in the restaurant industry, and helped grow my family's sip and paint studio into a thriving business. My goal is to bring my full self to an inclusive, authentic, and creative software development team where I can continue to learn and grow in this field. And when I'm not pushing up code, you can most likely find me with my schnauzer, Albus, listening to a Brené Brown podcast, or geeking out on home design & vintage finds."
 }
 
