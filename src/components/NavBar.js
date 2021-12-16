7372205ff5a275b1429eed1dc0b1447f85b753c8
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-12-16 15:58:03 -0500

    Remove active and scroll on Nav and add blur effect

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,14 @@
 import { HashLink } from 'react-router-hash-link';
 import { Link, NavLink, useLocation } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 import logo from '../images/logo.png';
 
 export const NavBar = () => {
 
-    const [navBar, setNavBar] = useState(false);
     const location = useLocation().pathname
-    
-    useEffect(() => {
-        changeBackground()
-        window.addEventListener("scroll", changeBackground)
-    })
 
-    const changeBackground = () => {
-        if (window.scrollY >= 35) {
-          setNavBar(true)
-        } else {
-          setNavBar(false)
-        }
-      }
 
     return (
-        <section className={navBar ? 'nav-active' : 'nav'}>
+        <section className='nav'>
             <Link to='/'>
                 <div className='nav-header'>
                     <img src={logo} alt='rachael carroll logo'></img>
