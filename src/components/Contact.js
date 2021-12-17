f958798429521b27faf35ab31dab26bd2b595b03
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-12-16 20:37:51 -0500

    Move contact styling to scss file

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,17 @@
-import Zoom from 'react-reveal/Zoom';
+// import Zoom from 'react-reveal/Zoom';
 import Flip from 'react-reveal/Flip';
 
 export const Contact = () => {
 
     return (
         <section className='contact'>
-            <Zoom right duration={1400} delay={100} distance='4rem'>
+            <Flip left cascade>
             <h2>Let’s start a conversation.</h2>
             <h2>Who knows, it could lead to the next great idea.</h2>
-            </Zoom>
+            </Flip>
 
-            <Flip left cascade delay={1500}>
-                <p style={{textAlign: 'center'}}>Find me on<span> <a href ='https://www.linkedin.com/in/rachaelcarroll/' target='_blank' rel='noreferrer'>LinkedIn</a> </span>
+            <Flip left cascade delay={800}>
+                <p className='contact-text'>Find me on<span> <a href ='https://www.linkedin.com/in/rachaelcarroll/' target='_blank' rel='noreferrer'>LinkedIn</a> </span>
                 or via email at <span><a href ='mailto:rachaelcarroll.m@gmail.com'>rachaelcarroll.m@gmail.com.</a></span> </p> 
             </Flip>
         </section>
