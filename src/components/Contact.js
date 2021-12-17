492fbddc6278a563833dbc53c88c50381f081a06
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-12-17 12:54:23 -0500

    Change contact transition

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,15 @@
-// import Zoom from 'react-reveal/Zoom';
+import Fade from 'react-reveal/Fade';
 import Flip from 'react-reveal/Flip';
 
 export const Contact = () => {
 
     return (
         <section className='contact'>
-            <Flip left cascade>
+            <Fade top>
             <h2>Let’s start a conversation.<br></br>Who knows, it could lead to the next great idea.</h2>
-            </Flip>
+            </Fade>
 
-            <Flip left cascade delay={800}>
+            <Flip left cascade delay={500}>
                 <p className='contact-text'>Find me on<span> <a href ='https://www.linkedin.com/in/rachaelcarroll/' target='_blank' rel='noreferrer'>LinkedIn</a> </span>
                 or via email at <span><a href ='mailto:rachaelcarroll.m@gmail.com'>rachaelcarroll.m@gmail.com.</a></span> </p> 
             </Flip>
