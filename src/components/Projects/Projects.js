fe44cccec077eb34e0bd2474751fac693188e3c5
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-12-17 12:54:33 -0500

    Add project header transition

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import { ProjectCard } from './ProjectCard';
 import { projectData } from './projectData';
 import Slide from 'react-reveal/Slide';
+import Fade from 'react-reveal/Fade';
 
 export const Projects = () => {
 
@@ -15,7 +16,9 @@ export const Projects = () => {
 
     return (
         <>
-        <h1 className='project-page-header'>A curated collection of projects that I'm personally proud of, or have helped shape me as a developer.</h1>
+        <Fade top>
+            <h1 className='project-page-header'>A curated collection of projects that I'm personally proud of, or have helped shape me as a developer.</h1>
+        </Fade>
         <Slide bottom duration={1000} delay={300} distance="0px">
             <section className='project-board'>
                 {projects}
