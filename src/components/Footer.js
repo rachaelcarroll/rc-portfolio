d6da22e6b274a92261b5744cd2a09ddfff52e98e
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-12-17 14:29:55 -0500

    Streamline single quoties

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ export const Footer = () => {
     return (
         <footer className='footer'>
         <section className='footer-links'>
-            <a href ='mailto:rachaelcarroll.m@gmail.com'><img src={"https://i.ibb.co/HCqybSP/mail-2.png"} className='footer-icon' alt='email icon'/></a>  
-            <a href ='https://github.com/rachaelcarroll' target='_blank' rel="noreferrer"><img src={"https://i.ibb.co/NyYsL14/github-2.png"} className='footer-icon' alt='github logo'/></a>  
-            <a href ='https://www.linkedin.com/in/rachaelcarroll/' target='_blank' rel="noreferrer"><img src={"https://i.ibb.co/fqvzWXm/linkedin-4.png"} className='footer-icon' alt='linkedin logo'/></a>  
+            <a href ='mailto:rachaelcarroll.m@gmail.com'><img src={'https://i.ibb.co/HCqybSP/mail-2.png'} className='footer-icon' alt='email icon'/></a>  
+            <a href ='https://github.com/rachaelcarroll' target='_blank' rel='noreferrer'><img src={'https://i.ibb.co/NyYsL14/github-2.png'} className='footer-icon' alt='github logo'/></a>  
+            <a href ='https://www.linkedin.com/in/rachaelcarroll/' target='_blank' rel='noreferrer'><img src={'https://i.ibb.co/fqvzWXm/linkedin-4.png'} className='footer-icon' alt='linkedin logo'/></a>  
         </section>
         <p>designed and developed by rachael carroll © {new Date().getFullYear()}</p>
         </footer>
