5491e93c12a05087f1dd2bdc9080df075e54f8b2
rachaelcarroll <rachaelcarroll.m@gmail.com>
2023-08-24 10:01:58 -0700

    Update bio section, remove headshot, update nav bar

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,19 +1,32 @@
 import React  from 'react';
+import { Link } from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
 import Flip from 'react-reveal/Flip';
+import resume from '../util/rachael-carroll.pdf'
 
 export const Contact = () => {
 
     return (
         <section className='contact'>
             <Fade top>
-            <h2>Let’s start a conversation.<br></br>Who knows, it could lead to the next great idea.</h2>
+            <h2>Let’s start a conversation.</h2>
             </Fade>
 
             <Flip left cascade delay={500}>
                 <p className='contact-text'>Find me on<span> <a href ='https://www.linkedin.com/in/rachaelcarroll/' target='_blank' rel='noreferrer'>LinkedIn</a> </span>
                 or via email at <span><a href ='mailto:rachaelcarroll.m@gmail.com'>rachaelcarroll.m@gmail.com.</a></span> </p> 
             </Flip>
+
+            <Link
+                to={resume}
+                download="rachael-carroll"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <button className='resume'>
+                    download resume
+                </button>
+            </Link>
         </section>
     )
 }
