8e6e308e290a466b16e736f1376f234ca95931b9
rachaelcarroll <rachaelcarroll.m@gmail.com>
2023-09-08 13:53:35 -0700

    Fix spacing on cards

--- src/pages/TechStack.js
+++ src/pages/TechStack.js
@@ -1,5 +1,5 @@
 import React  from 'react';
-import Fade from "react-reveal";
+import { Fade, Slide } from "react-reveal";
 import { techData } from "../util/techData";
 import { TechCard } from "../components/TechCard";
 
@@ -7,19 +7,19 @@ export const TechStack = () => {
   return (
     <section className="tech-container">
       <Fade bottom>
-        <section className="fe-be">
           {techData.map(data => 
           (
-            <TechCard 
-              key={data.type}
-              type={data.type} 
-              logo={data.logo} 
-              summary={data.summary}
-              stack={data.stack}
-            />
+            <Slide right>
+              <TechCard 
+                key={data.type}
+                type={data.type} 
+                logo={data.logo} 
+                summary={data.summary}
+                stack={data.stack}
+              />
+            </Slide>
           )
           )}
-        </section>
       </Fade>
     </section>
   );
